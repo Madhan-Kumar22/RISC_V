4f3dc9791db05ffa871045df3ea190e2b610b2cb
Vinesh S Talpankar <69723722+Vinesh-T@users.noreply.github.com>
2021-02-19 19:12:21 +0530

    Delete 2 directory

--- 2/2.asm
+++
@@ -1,6 +0,0 @@
-main:   
-	li a7, 4	# assigning call number to print string
-	la a0, str	# a0 keeps the address of the string
-	ecall		# System call
-.data
-str:      .string  "Hello World!\n" # String to print
